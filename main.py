c9aedeef137c580c3f7fbc0a769556f84169cae8
Christine Kovac <ckovac47@gmail.com>
2018-10-23 20:10:45 -0500

    finally done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 import cgi
 
 
@@ -10,7 +10,7 @@ app.config['DEBUG'] = True
 def display_form():
     return render_template('Index.html')
 
-@app.route('/verify', methods=['POST'])
+@app.route('/', methods=['POST'])
 def verify():
 
     username = request.form['username'] 
@@ -18,20 +18,23 @@ def verify():
     verify = request.form['verify']
     email = request.form['email']
 
-    username_error = " "
-    password_error = " "
-    verify_error = " "
-    email_error = " "
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
 
-    if len(username) < 3 and len(username) >20 or username == " ":
+    if len(username) < 3 or len(username) >20 or " " in username:
         username_error = "Username must be 3 - 20 characters and No spaces allowed please"
 
 
-    if len(password)  < 3 and len(password) >20 or password == " ":
+    if len(password)  < 3 or len(password) >20 or " " in password:
         password_error = "Password must be 3 - 20 characters and No spaces allowed please"
         password = ''
 
-  
+    if len(verify)  < 3 or len(verify) >20 or " " in verify:
+        verify_error = "Password must be 3 - 20 characters and No spaces allowed please"
+        verify = ''    
+          
     if verify == " ":
         verify_error = "Password is required." 
         verify = '' 
@@ -40,22 +43,23 @@ def verify():
         verify_error = "Password and verify must match."
         verify = ''
 
-    if email != " " and len(email) < 3 and len(email) > 20:
+    if email != "" and len(email) < 3 or len(email) > 20 or " " in email:
         email_error = "Email must be 3 - 20 characters"          
-    elif email != " " and ("@" not in email or "." not in email):
-            email_error = "Email must contain an @ sign and Email must contain a ."
+    elif email != "" and ("@" not in email or "." not in email):
+        email_error = "Email must contain an @ sign and Email must contain a ."
 
       
     if not username_error and not password_error and not verify_error and not email_error: 
-        return render_template('Welcome.html', username = username)
+        username=username
+        return redirect('/Welcome?username={0}'.format(username))
     else:
         return render_template('Index.html', username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, email=email)
     #hello  #goodbye
 
-    @app.route('/Welcome')
-    def Welcome():
-        username = request.args.get('username')
-        return render_template('Welcome.html', username=username)
+@app.route('/Welcome')
+def Welcome():
+    username = request.args.get('username')
+    return render_template('Welcome.html', username=username)
 
 if __name__ == "__main__":
     app.run()
